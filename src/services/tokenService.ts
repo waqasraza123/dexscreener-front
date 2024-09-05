e7352c833ddfa5c0a7f04d103fd504194fb9ea0d
Waqas Raza <waqasraza123@gmail.com>
2024-09-05 19:36:22 +0500

    load data from supabase

--- src/services/tokenService.ts
+++ src/services/tokenService.ts
@@ -3,6 +3,8 @@ import axios from 'axios';
 const API_URL = process.env.REACT_APP_API_BASE_URL;
 
 export const fetchTokens = async (chainId: string) => {
-	const response = await axios.get(`${API_URL}/api/dex/tokens?chainId=${chainId}`);
+	const response = await axios.get(`${API_URL}/api/dex/tokens`, {
+        params: { chainId },
+      });
 	return response.data;
 };
