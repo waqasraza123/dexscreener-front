a59ea87994e0a7af4a51ce4c09b08a89e872df3e
Waqas Raza <waqasraza123@gmail.com>
2024-09-20 23:08:45 +0500

    fixed loading spinner issue

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { ReactComponent as LoadingSpinnerIcon } from '../utils/loading_spinner.svg';
+import loadingSpinnerURL from '../utils/loading_spinner.svg';
 
 const LoadingSpinner: React.FC = () => {
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50">
       <div className="absolute inset-0 bg-gray-800 opacity-50"></div>
-      <LoadingSpinnerIcon className="w-16 h-16" />
+      <img src={loadingSpinnerURL} alt="Loading Spinner" className="w-16 h-16" />
     </div>
   );
 };
